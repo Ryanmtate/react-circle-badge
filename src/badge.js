3663271d6affcac05aa25cd6ca602de569c90ac5
Ryan Tate <ryan.michael.tate@gmail.com>
2018-01-08 09:57:34 -0800

    Fixing spacing

--- src/badge.js
+++ src/badge.js
@@ -8,21 +8,20 @@ export default class Badge extends Component {
   render() {
     let { fill, width, height, style, opacity, name } = this.props
 
-
     const initials = name.split(" ").map((w) => {
       return w[0].toUpperCase()
     }).concat()
 
-		opacity = opacity ? opacity : '0'
-		width = width ? width : '100%'
-		height = height ? height : '100%'
-		style = style ? style : null
+    opacity = opacity ? opacity : '0'
+    width = width ? width : '100%'
+    height = height ? height : '100%'
+    style = style ? style : null
 
-		return (
-			<svg width={width} height={height} viewBox="0 0 400 400" style={style} >
-				<circle cx="200" cy="200" r="42" fill={fill}/>
+    return (
+      <svg width={width} height={height} viewBox="0 0 400 400" style={style} >
+        <circle cx="200" cy="200" r="42" fill={fill}/>
         <text x="170" y="215" fontSize="50px" fontWeight={200} fill="#fff">{initials}</text>
-			</svg>
-		)
+      </svg>
+    )
   }
 }
